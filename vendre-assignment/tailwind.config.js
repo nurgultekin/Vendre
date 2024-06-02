3cf530999f80a27c0d8ebb60a2ba47f2bc76937f
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2024-06-02 13:05:52 +0200

    update tailwind configuration

--- vendre-assignment/tailwind.config.js
+++ vendre-assignment/tailwind.config.js
@@ -3,8 +3,11 @@ const forms = require('@tailwindcss/forms');
 const aspectRatio = require('@tailwindcss/aspect-ratio');
 
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   theme: {
     extend: {},
   },
